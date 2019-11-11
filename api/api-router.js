0a074006034b477595504915d528006af07e4f78
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-11-11 13:22:23 -0800

    test server

---
+++ api/api-router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+const bcrypt = require('bcryptjs');
+
+// const authRouter = require('../auth/auth-router.js');
+// const usersRouter = require('../users/user-router.js');
+
+// router.use('/auth', authRouter);
+// router.use('/users', usersRouter);
+
+// router.get('/', (req, res) => {
+//    res.json({ api: "It's alive" });
+// });
+
+// router.post('/hash', (req, res) => {
+//    const password = req.body;
+
+//    const hash = bcrypt.hashSync('password', 12);
+
+//    res.status(200).json({ password, hash });
+// });
+
+module.exports = router;
